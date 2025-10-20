dc2083573dd3f366340b0c356ff5d86506b949e1
VvAlexandrevV <luis.alexandre@outlook.fr>
2025-10-20 12:29:06 +0200

    Première version AgencEco

---
+++ script.js
@@ -0,0 +1,10 @@
+const age = 12;
+console.log(age);
+if(age>= 18) {
+    console.log("vous etes majeur");
+} else { // else=sinon possibilité de combiné else et if 
+    console.log("vous etes mineur");
+}
+
+
+//ne pas tenir compte e code
